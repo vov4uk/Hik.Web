0f6a5e8f7e309c2b7b448d8866d64574204181f1
vov4uk <khmel@outlook.com>
2022-02-18 17:59:34 +0200

    Revert startup fie

--- src/Hik.Web/Program.cs
+++ src/Hik.Web/Program.cs
@@ -2,14 +2,10 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using Hik.DataAccess;
-using Hik.Web.Scheduler;
-using Microsoft.AspNetCore.Builder;
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Hik.Web
@@ -18,68 +14,66 @@ namespace Hik.Web
     {
         public static void Main(string[] args)
         {
+            // https://dotnetcoretutorials.com/2018/09/12/hosting-an-asp-net-core-web-application-as-a-windows-service/
             var isService = !(Debugger.IsAttached || args.Contains("--console"));
+            var builder = CreateHostBuilder(isService, args.Where(arg => arg != "--console").ToArray());
 
-            StartupNet6(isService, args.Where(arg => arg != "--console").ToArray());
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule?.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                builder.UseContentRoot(pathToContentRoot);
+            }
+
+            var host = builder.Build();
+
+            if (isService)
+            {
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
         }
 
-        public static void StartupNet6(bool isService, string[] args)
+        public static IWebHostBuilder CreateHostBuilder(bool isService, string[] args)
         {
             var env = isService ? "Production" : "Development";
             var config = new ConfigurationBuilder()
-                .SetBasePath(AssemblyDirectory)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
-                .AddCommandLine(args)
-                .Build();
+            .SetBasePath(AssemblyDirectory)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
+            .AddCommandLine(args)
+            .Build();
 
             AutofacConfig.RegisterConfiguration(config);
 
             var port = config.GetSection("Hosting:Port").Value;
 
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddRazorPages();
-            builder.Services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(config.GetConnectionString("HikConnectionString"), options =>
+            return new WebHostBuilder()
+                .UseKestrel()
+                .UseEnvironment(env)
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
                 {
-                    options.MigrationsAssembly("Hik.DataAccess.dll");
-                });
-            });
-            builder.Host.UseEnvironment(env);
-            builder.Logging.ClearProviders();
-            builder.Logging.AddConsole();
-            builder.Logging.AddEventLog();
-
-            if (isService)
-            {
-                builder.Host.UseWindowsService();
-                builder.WebHost.UseContentRoot(AssemblyDirectory);
-            }
-
-            var app = builder.Build();
-
-            app.UseDeveloperExceptionPage();
-            app.UseStaticFiles();
-            app.UseRouting();
-            app.MapRazorPages();
-            app.Urls.Add($"http://+:{port}");
-
-            var quartz = new QuartzStartup(config);
-
-            app.Lifetime.ApplicationStarted.Register(quartz.Start);
-            app.Lifetime.ApplicationStopping.Register(quartz.Stop);
-
-            app.Run();
+                    logging.ClearProviders();
+                    logging.AddEventLog();
+                    logging.AddConsole();
+                })
+                .UseUrls($"http://+:{port}");
         }
 
         private static string AssemblyDirectory
         {
             get
             {
-                var pathToExe = Process.GetCurrentProcess().MainModule?.FileName;
-                return Path.GetDirectoryName(pathToExe);
+                string codeBase = Assembly.GetExecutingAssembly().Location;
+                UriBuilder uri = new(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
             }
         }
     }
