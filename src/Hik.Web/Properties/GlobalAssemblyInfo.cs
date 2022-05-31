42d7b9d3fa77b5922b67c724d40f594cc4f8d9d6
vov4uk <khmel@outlook.com>
2022-05-31 18:02:28 +0300

    Remove _FilesSearch

--- src/Hik.Web/Properties/GlobalAssemblyInfo.cs
+++ src/Hik.Web/Properties/GlobalAssemblyInfo.cs
@@ -1,9 +1,9 @@
-//2022.5.30.4
+//2022.5.31.1
 using System.Reflection;
 
 [assembly: AssemblyCompany("Hik.Web")]
 [assembly: AssemblyProduct("Hik.Web")]
 [assembly:   AssemblyTitle("Hik.Web")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("2022.5.30.4")]
-[assembly:     AssemblyVersion("2022.5.30.4")]
+[assembly: AssemblyFileVersion("2022.5.31.1")]
+[assembly:     AssemblyVersion("2022.5.31.1")]
