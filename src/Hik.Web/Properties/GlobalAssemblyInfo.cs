95a44bf5c38afc1744b4a02f75737711257da9e2
vov4uk <khmel@outlook.com>
2022-06-06 10:09:33 +0300

    Fix issue with login - after click cancel - it redirects to home page

--- src/Hik.Web/Properties/GlobalAssemblyInfo.cs
+++ src/Hik.Web/Properties/GlobalAssemblyInfo.cs
@@ -1,9 +1,9 @@
-//2022.5.31.1
+//2022.6.3.2
 using System.Reflection;
 
 [assembly: AssemblyCompany("Hik.Web")]
 [assembly: AssemblyProduct("Hik.Web")]
 [assembly:   AssemblyTitle("Hik.Web")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("2022.5.31.1")]
-[assembly:     AssemblyVersion("2022.5.31.1")]
+[assembly: AssemblyFileVersion("2022.6.3.2")]
+[assembly:     AssemblyVersion("2022.6.3.2")]
