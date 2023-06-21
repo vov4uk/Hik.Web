e4074ad3856319d2edb63951b5ff7fcebbbaa545
Volodymyr Khmelovskyi <khmelovskyi@outlook.com>
2023-06-21 14:56:36 +0300

    rename file

--- src/Hik.Web.Queries/QueryHandler.cs
+++ src/Hik.Web.Queries/QueryHandler.cs
@@ -16,7 +16,7 @@ namespace Hik.Web.Queries
                 .MinimumLevel.Information()
                 .Enrich.FromLogContext()
                 .WriteTo.Console()
-                .WriteTo.File("Logs\\QueryHandler.txt", rollingInterval: RollingInterval.Day)
+                .WriteTo.File("Logs\\QueryHandler_.txt", rollingInterval: RollingInterval.Day)
                 .WriteTo.Seq("http://localhost:5341")
                 .CreateLogger();
         }
